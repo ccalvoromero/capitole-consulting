3ae6716fd77970efb9a2acc77ea8de9a1329658f
Cristopher Calvo Romero <ccromero@veritran.com>
2021-07-31 01:36:36 -0300

    work in progress: starting de project with dependencies and arq hex packaging

---
+++ src/main/java/com/capitoleconsulting/CapitoleConsultingApplication.java
@@ -0,0 +1,13 @@
+package com.capitoleconsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitoleConsultingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitoleConsultingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capitoleconsulting/domain/CapitoleConsultingTest.java
@@ -0,0 +1,13 @@
+package com.capitoleconsulting.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class CapitoleConsultingTest {
+
+   @Test
+    public void a_random_test() {
+        Assertions.assertEquals(1, 2-1);
+    }
+
+}
